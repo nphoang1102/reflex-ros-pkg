25cdaec6464ca3d89c970263c19a8d75b9c7a043
Hoang Nguyen <nphoang1102@gmail.com>
2017-09-20 09:30:48 -0400

    I think the autocalibration process should have a class of its own like the other sources, rather than just a script, but I'm also curious if this works or not

--- reflex/src/sf_auto_calib.py
+++ reflex/src/sf_auto_calib.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+# 
+# Script for autocalibration of the reflex SF hand
+# 
+
+__author__ = 'Hoang Nguyen'
+__maintainer__ = 'Hoang Nguyen'
+__email__ = 'nphoang1102@gmail.com'
 
 
 from math import pi, cos
@@ -12,37 +18,61 @@ from reflex_msgs.msg import VelocityCommand
 # from reflex_msgs.msg import ForceCommand
 from reflex_msgs.msg import Hand
 
+# Class for Reflex SF hand autocalibration
+class AutoCalibrate(object):
 
-hand_state = Hand()
+    # Initialize class method
+    def __init__(self):
 
-def main():
+        # First thing first, this needs a node name
+        rospy.init_node('SFautoCalibration')
 
-    rospy.init_node('SFautoCalibration')
+        # Overload tracking dictionary
+        self.motors = {
+            0 : None, # Finger 1
+            1 : None, # Finger 2
+            2 : None, # Finger 3
+            3 : None # Preshape
+        }
 
-    # This collection of publishers can be used to command the hand
-    command_pub = rospy.Publisher('/reflex_sf/command', Command, queue_size=1)
-    pos_pub = rospy.Publisher('/reflex_sf/command_position', PoseCommand, queue_size=1)
-    vel_pub = rospy.Publisher('/reflex_sf/command_velocity', VelocityCommand, queue_size=1)
-    # force_pub = rospy.Publisher('/reflex_sf/command_motor_force', ForceCommand, queue_size=1)
+        # Start with not updated load value on motors
+        self.load_updated = False
 
-    # Constantly capture the current hand state
-    rospy.Subscriber('/reflex_sf/hand_state', Hand, hand_state_cb)
+        # Fetching user config params from .yaml file
+        self.threshold = {
+            0: rospy.get_param("reflex_sf_f1/overload_threshold")
+            1: rospy.get_param("reflex_sf_f2/overload_threshold")
+            2: rospy.get_param("reflex_sf_f3/overload_threshold")
+            3: rospy.get_param("reflex_sf_f4/overload_threshold")
+        }
 
-    # Demonstration of velocity control - variable closing speed
-    raw_input("== When ready to open and close fingers with velocity control, hit [Enter]\n")
-    for i in range(3):
-        pos_pub.publish(PoseCommand(f1=i, f2=i, f3=i, preshape=0.0))
-        rospy.sleep(2.0)
-        setpoint = 5.0 - (i * 2.25)
-        vel_pub.publish(VelocityCommand(f1=setpoint, f2=setpoint, f3=setpoint, preshape=0.0))
-        rospy.sleep(7.0 - setpoint)
-    raw_input("...\n")
-    pos_pub.publish(PoseCommand())
+        # Subscribe to the hand_state topic and update the load reading
+        rospy.Subscriber('/reflex_sf/hand_state', Hand, self._loadupdate_cb)
 
-def hand_state_cb(data):
-    global hand_state
-    hand_state = data
-    # rospy.loginfo(hand_state.motor)
+        # Initialize all of the topics to publish to
+        self.command_pub = rospy.Publisher('/reflex_sf/command', Command, queue_size=1)
+        self.pos_pub = rospy.Publisher('/reflex_sf/command_position', PoseCommand, queue_size=1)
+        self.vel_pub = rospy.Publisher('/reflex_sf/command_velocity', VelocityCommand, queue_size=1)
 
+        # Wait for updated motor value then start autocalibration process
+        while(not self.load_updated):    
+        self._auto_calib()
+
+    # Callback for updating the load on each motors
+    def _loadupdate_cb(data):
+        self.motors[0] = data.motor[0].load
+        self.motors[1] = data.motor[1].load
+        self.motors[2] = data.motor[2].load
+        self.motors[3] = data.motor[3].load
+
+        # Inform that we have updated the new load value for each motors
+        if (not self.load_updated):
+            self.load_updated = True
+
+    # Autocalibration process here
+    def _auto_calib():
+
+# Run the calibration process in main
 if __name__ == '__main__':
-    main()
+    calibration_process = AutoCalibrate()
+    rospy.spin()
